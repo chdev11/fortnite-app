59b5298a30d34ab95c8514f952ecf7c104e3703b
primeinterway <carlos.assofra@hotmail.com>
2022-05-30 16:47:08 -0300

    [ADD] Env configurations

--- lib/app/modules/home/external/datasources/package_datasource_impl.dart
+++ lib/app/modules/home/external/datasources/package_datasource_impl.dart
@@ -3,6 +3,7 @@ import 'package:fortnite_app/app/modules/home/domain/entities/package.dart';
 import 'package:fortnite_app/app/modules/home/domain/errors/package_error.dart';
 import 'package:fortnite_app/app/modules/home/infra/datasources/package_datasource.dart';
 import 'package:fortnite_app/app/modules/home/infra/models/package_model.dart';
+import 'package:fortnite_app/env.dart';
 
 class PackageDatasourceImpl implements IPackageDatasource {
   final Dio client;
@@ -11,11 +12,9 @@ class PackageDatasourceImpl implements IPackageDatasource {
 
   @override
   Future<List<Package>> fetchPackages() async {
-    final result = await client.get('https://fortnite-api.com/v2/shop/br',
-        queryParameters: {'language': 'pt-BR'},
-        options: Options(headers: {
-          'Authorization': '1f53725b-4121-4bb9-9610-7db4271f3614'
-        }));
+    final result = await client.get(baseUrl,
+        queryParameters: {'language': apiLanguage},
+        options: Options(headers: {'Authorization': apiSecret}));
     if (result.statusCode == 200) {
       try {
         return (result.data['data']['featured']['entries'] as List)
